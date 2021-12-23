af196c8c812a971a069119673ad8f2ca38d783eb
Mert Barut <mbarut@c2s12d59.42wolfsburg.de>
2021-12-23 13:52:56 +0100

    tidied up the repository

--- src/cub_render.c
+++ src/cub_render.c
@@ -6,12 +6,11 @@
 /*   By: mbarut <mbarut@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/22 15:55:04 by mbarut            #+#    #+#             */
-/*   Updated: 2021/12/22 22:24:59 by mbarut           ###   ########.fr       */
+/*   Updated: 2021/12/23 13:27:00 by mbarut           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub.h"
-#include <endian.h>
 
 extern	int	map[24][24];
 
