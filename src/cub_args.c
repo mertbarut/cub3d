8c03f9233551b35c9be078e7715897f0ea3d1c1d
DimitrisMylonas <dimitris20cen@gmail.com>
2022-01-04 11:27:54 +0200

    added error message for wrong input

--- src/cub_args.c
+++ src/cub_args.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub_args.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbarut <mbarut@student.42wolfsburg.de>     +#+  +:+       +#+        */
+/*   By: dmylonas <dmylonas@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/31 14:40:11 by mbarut            #+#    #+#             */
-/*   Updated: 2022/01/02 16:14:08 by mbarut           ###   ########.fr       */
+/*   Updated: 2022/01/04 11:25:23 by dmylonas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,20 @@ void	cub_args(int argc, char **argv)
 	if (argc != 2)
 	{
 		printf("Error\n");
+		printf("Game accepts 1 input, no more, no less, thank you :)\n");
 		exit(EXIT_FAILURE);
 	}
 	if ((ft_strlen(argv[1]) < 4) || (!ft_strrchr(argv[1], '.')))
 	{
 		printf("Error\n");
+		printf("Wrong input format\n");
 		exit(EXIT_FAILURE);
 	}
 	extension = ft_strrchr(argv[1], '.');
 	if (ft_strcmp(extension, ".cub") != 0)
 	{
 		printf("Error\n");
+		printf("Wrong input format\n");
 		exit(EXIT_FAILURE);
 	}
 }
